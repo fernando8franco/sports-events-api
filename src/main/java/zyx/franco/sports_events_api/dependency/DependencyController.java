501e1025c175aa0f7b253289d75b8cf0f7e2f27f
fernando8franco <francoduranluisfernando@gmail.com>
2024-12-07 16:43:30 -0600

    save dependency method & dependencyDTO modified

--- src/main/java/zyx/franco/sports_events_api/dependency/DependencyController.java
+++ src/main/java/zyx/franco/sports_events_api/dependency/DependencyController.java
@@ -1,11 +1,20 @@
 package zyx.franco.sports_events_api.dependency;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/v1/dependencies")
@@ -14,15 +23,6 @@ public class DependencyController {
 
     public DependencyController(DependencyService dependencyService) {
         this.dependencyService = dependencyService;
-    }
-
-    @GetMapping("/{dependencyId}")
-    public ResponseEntity<Dependency> findDependencyById(@PathVariable Integer dependencyId) {
-        Dependency dependency = dependencyService.findById(dependencyId);
-
-        return (dependency != null)
-                ? ResponseEntity.ok(dependency)
-                : ResponseEntity.notFound().build();
     }
 
     @PostMapping
@@ -37,5 +37,42 @@ public class DependencyController {
                 .toUri();
 
         return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("/{dependencyId}")
+    public ResponseEntity<Dependency> findDependencyById(@PathVariable Integer dependencyId) {
+        Dependency dependency = dependencyService.findById(dependencyId);
+
+        return (dependency != null)
+                ? ResponseEntity.ok(dependency)
+                : ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        HashMap<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors()
+                .forEach(error -> {
+                    String fieldName = ((FieldError) error).getField();
+                    String errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName, errorMessage);
+                });
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> handleMessageNotReadableException(HttpMessageNotReadableException ex) {
+        if (!(ex.getCause() instanceof InvalidFormatException ifx))
+            return ResponseEntity.badRequest().body(Collections.singletonMap("message", ex.getMessage()));
+
+        if (!(ifx.getTargetType() != null && ifx.getTargetType().isEnum()))
+            return ResponseEntity.badRequest().body(Collections.singletonMap("message", ex.getMessage()));
+
+        return ResponseEntity.badRequest().body(Collections.singletonMap(
+                "category",
+                String.format("Invalid enum value: '%s'. The value must be one of: %s.",
+                ifx.getValue(), Arrays.toString(ifx.getTargetType().getEnumConstants()))
+        ));
     }
 }
--- src/main/java/zyx/franco/sports_events_api/dependency/DependencyDTO.java
+++ src/main/java/zyx/franco/sports_events_api/dependency/DependencyDTO.java
@@ -1,11 +1,12 @@
 package zyx.franco.sports_events_api.dependency;
 
 import jakarta.validation.constraints.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 public record DependencyDTO(
-        @NotBlank(message = "The dependency name must not be null or blank")
-        @Size(min = 5, max = 150, message = "The dependency name must be between 5 and 200 characters")
+        @Size(min = 5, max = 150, message = "The dependency name must be between 5 and 150 characters")
         String name,
+        @NotNull(message = "The category must not be null")
         DependencyCategory category
 ) {
 }

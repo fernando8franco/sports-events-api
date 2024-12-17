6c2645b23b8151ce4bfbbb3c803c595d915459c3
fernando8franco <francoduranluisfernando@gmail.com>
2024-12-17 16:48:00 -0600

    sport find all, find by id, update & delete routes added

--- src/main/java/zyx/franco/sports_events_api/sport/SportController.java
+++ src/main/java/zyx/franco/sports_events_api/sport/SportController.java
@@ -1,14 +1,15 @@
 package zyx.franco.sports_events_api.sport;
 
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/sports")
@@ -31,5 +32,53 @@ public class SportController {
                 .toUri();
 
         return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SportResponseDTO>> findAllSports(
+            @PageableDefault(page = 1, size = 5) Pageable pageable,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "true") boolean ascending
+    ) {
+        Page<SportResponseDTO> sportResponseDTOPage = sportService.findAllSports(
+                pageable,
+                sortBy,
+                ascending
+        );
+        return ResponseEntity.ok(sportResponseDTOPage.getContent());
+    }
+
+    @GetMapping("/{sportId}")
+    public ResponseEntity<SportDTO> findSportById(
+            @PathVariable Integer sportId
+    ) {
+        SportDTO sportDTO = sportService.findSportById(sportId);
+        return ResponseEntity.ok(sportDTO);
+    }
+
+    @PutMapping("/{sportId}")
+    public ResponseEntity<Void> updateSport(
+            @PathVariable Integer sportId,
+            @Valid @RequestBody SportDTO sportDTO
+    ) {
+        sportService.findSportById(sportId);
+
+        Sport updateSport = SportMapper.toSportEntity(sportDTO);
+        updateSport.setId(sportId);
+
+        sportService.updateSport(updateSport);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{sportId}")
+    public ResponseEntity<Void> deleteSport(
+            @PathVariable Integer sportId
+    ) {
+        sportService.findSportById(sportId);
+
+        sportService.deleteSport(sportId);
+
+        return ResponseEntity.noContent().build();
     }
 }
